0a5dbe600017a52df0021a77f3ad1fc05bcc550f
Henning Treu <henning.treu@googlemail.com>
2015-09-11 16:20:31 +0200

    fix SwitchItem test

--- test/SwitchItemTest.js
+++ test/SwitchItemTest.js
@@ -1,36 +1,32 @@
 import should from 'should';
-import Types from 'HAP-NodeJS';
-var types = Types.types;
+import HAPNodeJS from 'HAP-NodeJS';
+
 import { SwitchItem } from '../lib/SwitchItem.js';
 
 process.env.NODE_ENV = 'test';
 
 function createSwitchItem() {
-  return new SwitchItem('switchItemName', '1', 'http://openhab.test/switchItem/state');
+  return new SwitchItem('switchItemName', 'http://openhab.test/switchItem', '1');
 }
 
 describe('SwitchItem', function () {
 
-  it('should contain two services', function () {
+  it('should contain AccessoryInformation & Lightbulb services', function () {
     let switchItem = createSwitchItem();
-    switchItem.should.have.property('services');
-    switchItem.services.should.have.length(2);
-  });
-
-  it('should contain AccessoryInformationService', function () {
-    let switchItem = createSwitchItem();
-    switchItem.services[0].sType.should.equal(types.ACCESSORY_INFORMATION_STYPE);
-  });
-
-  it('should contain SwitchService', function () {
-    let switchItem = createSwitchItem();
-    switchItem.services[1].sType.should.equal(types.SWITCH_STYPE);
+    switchItem.should.have.property('accessory');
+    switchItem.accessory.getService(HAPNodeJS.Service.AccessoryInformation)
+      .should.not.be.empty;
+    switchItem.accessory.getService(HAPNodeJS.Service.Lightbulb)
+      .should.not.be.empty;
   });
 
   it('should have set the correct name', function () {
     let switchItem = createSwitchItem();
-    switchItem.services[0].characteristics[0].initialValue.should.equal('switchItemName');
-    switchItem.services[1].characteristics[0].initialValue.should.equal('switchItemName');
+    let accessory = switchItem.accessory;
+    accessory.getService(HAPNodeJS.Service.AccessoryInformation)
+    .getCharacteristic(HAPNodeJS.Characteristic.Name).value.should.equal('switchItemName');
+    accessory.getService(HAPNodeJS.Service.Lightbulb)
+    .getCharacteristic(HAPNodeJS.Characteristic.Name).value.should.equal('switchItemName');
   });
 
 });
